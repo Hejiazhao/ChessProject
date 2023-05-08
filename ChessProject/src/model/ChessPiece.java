94fb50f7e6257e0d2d074ede0475f92a7cc9e75e
Hejiazhao <132820009+Hejiazhao@users.noreply.github.com>
2023-05-08 19:17:21 +0800

    Add files via upload

---
+++ ChessProject/src/model/ChessPiece.java
@@ -0,0 +1,32 @@
+package model;
+
+
+public class ChessPiece {
+    // the owner of the chess
+    private PlayerColor owner;
+
+    // Elephant? Cat? Dog? ...
+    private String name;
+    private int rank;
+
+    public ChessPiece(PlayerColor owner, String name, int rank) {
+        this.owner = owner;
+        this.name = name;
+        this.rank = rank;
+    }
+    public int getRank(){return rank;}
+
+
+    public boolean canCapture(ChessPiece target) {
+        // TODO: Finish this method!
+        return this.rank >= target.rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PlayerColor getOwner() {
+        return owner;
+    }
+}
