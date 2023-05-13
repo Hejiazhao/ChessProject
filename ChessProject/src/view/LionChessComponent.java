3c6466a9f326631c69bfaa042310965846e668d8
Caiyidie <132819879+Caiyidie@users.noreply.github.com>
2023-05-13 11:33:45 +0800

    Add files via upload

---
+++ ChessProject/src/view/LionChessComponent.java
@@ -0,0 +1,29 @@
+package view;
+
+import model.PlayerColor;
+
+import java.awt.*;
+
+//“鼠”棋子的代码
+public class LionChessComponent extends AnimalChessComponent{
+    public LionChessComponent(PlayerColor owner, int size){
+        super(owner,size);
+        setLocation(0,0);
+        this.animalType=AnimalType.Mouse;
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Font font = new Font("楷体", Font.PLAIN, getWidth() / 2);
+        g2.setFont(font);
+        g2.setColor(owner.getColor());
+        g2.drawString("狮", getWidth() / 4, getHeight() * 5 / 8); // FIXME: Use library to find the correct offset.
+        if (isSelected()) { // Highlights the model if selected.
+            g.setColor(owner.getColor());
+            g.drawOval(0, 0, getWidth() , getHeight());
+        }
+    }
+
+}
