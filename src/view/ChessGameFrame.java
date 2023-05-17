f5059f4e82ff7b7fbd38684bbd8d0b9f8485e3d2
Caiyidie <12210660@mail.sustech.edu.cn>
2023-05-17 14:48:22 +0800

    emm

--- src/view/ChessGameFrame.java
+++ src/view/ChessGameFrame.java
@@ -91,7 +91,7 @@ public class ChessGameFrame extends JFrame {
 
     private void Save(GameController gameController,String name){
         try {
-            File newFile = new File("Save/"+name+".txt");
+            File newFile = new File("Save"+name+".txt");
             if (newFile.createNewFile()){
                 FileWriter fileWriter = new FileWriter(newFile);
                 BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
@@ -162,7 +162,9 @@ public class ChessGameFrame extends JFrame {
         button.setFont(new Font("宋体", Font.BOLD, 20));
         add(button);
     }
+
 }
+
 
 
 
