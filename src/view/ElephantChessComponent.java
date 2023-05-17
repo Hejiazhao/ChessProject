0c64c78177023cab590dc19f25c9807fb949b106
Caiyidie <12210660@mail.sustech.edu.cn>
2023-05-17 16:27:36 +0800

    emm

--- src/view/ElephantChessComponent.java
+++ src/view/ElephantChessComponent.java
@@ -1,34 +1,26 @@
 package view;
 
 import model.PlayerColor;
-
+import javax.swing.*;
 import java.awt.*;
 
 
 //将原来“象”棋子的公有功能拆分后，私有的功能
 public class ElephantChessComponent extends AnimalChessComponent{
-
+private Image elephantImage;
     public ElephantChessComponent(PlayerColor owner, int size){
         super(owner,size);
         setLocation(2,6);
         this.animalType=AnimalType.Elephant;
+        this.elephantImage=new ImageIcon("C:\\Users\\蔡卓茜\\IdeaProjects\\ChessProject\\resource\\Elephant-blue.png").getImage();
     }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        Font font = new Font("楷体", Font.PLAIN, getWidth()/2);
-
-        g2.setFont(font);
-        g2.setColor(owner.getColor());
-        g2.drawString("象", getWidth() / 4, getHeight() * 5 / 8);
-
-        // FIXME: Use library to find the correct offset.
-        if (isSelected()) { // Highlights the model if selected.
-            g.setColor(owner.getColor());
-            g.drawOval(0, 0, getWidth() , getHeight());
-        }
-    }
+@Override
+protected void paintComponent(Graphics g){
+        super.paintComponents(g);
+        Graphics2D g2=(Graphics2D) g;
+        g2.drawImage(elephantImage,0,0,null);
 }
+
+}
+
+
